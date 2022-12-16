4f186e3267bd8cef6186b4e3e88b11ae61647a1f
Marcel Joss <marceljoss@hotmail.ch>
2022-12-16 21:35:44 +0100

    Day 16 Part 1

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,7 +1,8 @@
+import kotlin.math.max
+
 fun main() {
     class Valve(val name: String, val flowRate: Int) {
         val connections: MutableList<Valve> = mutableListOf()
-        var isClosed = true
     }
 
     val valveRegex = Regex("""Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.*)""")
@@ -17,7 +18,9 @@ fun main() {
         return valvesWithConnections.mapValues { it.value.first }
     }
 
-    fun findPath(start: Valve, end: Valve): List<Valve> {
+    val pathCache = mutableMapOf<Pair<Valve, Valve>, List<Valve>>()
+
+    fun findPath(start: Valve, end: Valve): List<Valve> = pathCache.computeIfAbsent(start to end) {
         val queue = ArrayDeque<Valve>()
         val explored = mutableSetOf(start)
         val parents = mutableMapOf<Valve, Valve>()
@@ -31,7 +34,7 @@ fun main() {
                     path.addFirst(current)
                     current = parents[current]
                 }
-                return path.toList()
+                return@computeIfAbsent path.toList()
             }
             v.connections.asSequence().filter { it !in explored }.forEach {
                 explored.add(it)
@@ -42,56 +45,42 @@ fun main() {
         error("no path found")
     }
 
-    fun pressurePerMinute(path: List<Valve>, remainingMinutes: Int): Int {
-        var minutes = remainingMinutes
-        var pressure = 0
-        for (v in path) {
-            if (v.isClosed && v.flowRate > 0) {
-                --minutes
-                pressure += v.flowRate * minutes
-            }
-            --minutes
-        }
-        return pressure / (remainingMinutes - minutes)
-    }
+    data class State(val currentValve: Valve, val remainingMinutes: Int, val pressure: Int, val remainingValves: Set<Valve>)
 
     fun part1(input: List<String>): Int {
         val valves = readValves(input)
-        val usefulValves = valves.values.filter { it.flowRate > 0 }.toMutableSet()
-        var start = valves.getValue("AA")
-        var remainingMinutes = 30
-        var releasedPressure = 0
-        while (remainingMinutes > 0 && usefulValves.isNotEmpty()) {
-            val path = usefulValves.asSequence()
-                .map { findPath(start, it) }
-                .maxBy { pressurePerMinute(it, remainingMinutes) }
-            for (v in path) {
-                println("Move to valve ${v.name}")
-                if (v.isClosed && v.flowRate > 0) {
-                    --remainingMinutes
-                    println("Open valve ${v.name}")
-                    releasedPressure += v.flowRate * remainingMinutes
-                    v.isClosed = false
+        val usefulValves = valves.values.filter { it.flowRate > 0 }.toSet()
+        var states = listOf(State(valves.getValue("AA"), 30, 0, usefulValves))
+        var maxReleasedPressure = 0
+        while (states.isNotEmpty()) {
+            states = states.flatMap { state ->
+                val (current, remainingMinutes, releasedPressure, remainingValves) = state
+                remainingValves.map {
+                    val path = findPath(current, it)
+                    if (path.size > remainingMinutes) {
+                        state.copy(remainingMinutes = 0)
+                    } else {
+                        val valve = path.last()
+                        val minutes = remainingMinutes - path.size
+                        val pressure = releasedPressure + valve.flowRate * minutes
+                        State(valve, minutes, pressure, remainingValves - valve)
+                    }
                 }
-                --remainingMinutes
             }
-            // one decrement too much
-            ++remainingMinutes
-            usefulValves -= path
-            start = path.last()
+            maxReleasedPressure = max(maxReleasedPressure, states.maxOf { it.pressure })
+            states = states.filter { it.remainingMinutes > 0 && it.remainingValves.isNotEmpty() }
         }
-        println(releasedPressure)
-        return releasedPressure
+        return maxReleasedPressure
     }
 
-//    fun part2(input: List<String>, maxCoordinate: Int): Long
+//    fun part2(input: List<String>): Int
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day16_test")
     check(part1(testInput) == 1651)
-//    check(part2(testInput, 20) == 56000011L)
+//    check(part2(testInput) == 1707)
 
     val input = readInput("Day16")
     println(part1(input))
-//    println(part2(input, 4_000_000))
+//    println(part2(input))
 }
