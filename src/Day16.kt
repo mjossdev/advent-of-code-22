61b723eed45403feddc12b6d8ce6a27e3578a059
Marcel Joss <marceljoss@hotmail.ch>
2022-12-17 02:31:35 +0100

    Day 16 Part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,3 +1,32 @@
+import kotlin.collections.ArrayDeque
+import kotlin.collections.List
+import kotlin.collections.Map
+import kotlin.collections.MutableList
+import kotlin.collections.Set
+import kotlin.collections.any
+import kotlin.collections.asSequence
+import kotlin.collections.associateBy
+import kotlin.collections.drop
+import kotlin.collections.emptyList
+import kotlin.collections.first
+import kotlin.collections.forEach
+import kotlin.collections.getValue
+import kotlin.collections.hashMapOf
+import kotlin.collections.isNotEmpty
+import kotlin.collections.last
+import kotlin.collections.listOf
+import kotlin.collections.map
+import kotlin.collections.mapValues
+import kotlin.collections.maxOfOrNull
+import kotlin.collections.minus
+import kotlin.collections.mutableListOf
+import kotlin.collections.mutableMapOf
+import kotlin.collections.mutableSetOf
+import kotlin.collections.plus
+import kotlin.collections.set
+import kotlin.collections.sortedByDescending
+import kotlin.collections.sumOf
+import kotlin.collections.toList
 import kotlin.math.max
 
 fun main() {
@@ -18,69 +47,101 @@ fun main() {
         return valvesWithConnections.mapValues { it.value.first }
     }
 
-    val pathCache = mutableMapOf<Pair<Valve, Valve>, List<Valve>>()
+    class PathFinder {
+        private val pathCache = hashMapOf<Pair<Valve, Valve>, List<Valve>>()
 
-    fun findPath(start: Valve, end: Valve): List<Valve> = pathCache.computeIfAbsent(start to end) {
-        val queue = ArrayDeque<Valve>()
-        val explored = mutableSetOf(start)
-        val parents = mutableMapOf<Valve, Valve>()
-        queue.addLast(start)
-        while (queue.isNotEmpty()) {
-            val v = queue.removeFirst()
-            if (v == end) {
-                val path = ArrayDeque<Valve>()
-                var current: Valve? = v
-                while (current != null) {
-                    path.addFirst(current)
-                    current = parents[current]
+        fun findPath(start: Valve, end: Valve): List<Valve> = pathCache.computeIfAbsent(start to end) {
+            val queue = ArrayDeque<Valve>()
+            val explored = mutableSetOf(start)
+            val parents = mutableMapOf<Valve, Valve>()
+            queue.addLast(start)
+            while (queue.isNotEmpty()) {
+                val v = queue.removeFirst()
+                if (v == end) {
+                    val path = ArrayDeque<Valve>()
+                    var current: Valve? = v
+                    while (current != null) {
+                        path.addFirst(current)
+                        current = parents[current]
+                    }
+                    return@computeIfAbsent path.toList()
                 }
-                return@computeIfAbsent path.toList()
+                v.connections.asSequence().filter { it !in explored }.forEach {
+                    explored.add(it)
+                    parents[it] = v
+                    queue.addLast(it)
+                }
             }
-            v.connections.asSequence().filter { it !in explored }.forEach {
-                explored.add(it)
-                parents[it] = v
-                queue.addLast(it)
-            }
+            error("no path found")
         }
-        error("no path found")
     }
 
-    data class State(val currentValve: Valve, val remainingMinutes: Int, val pressure: Int, val remainingValves: Set<Valve>)
+    class ObjectPool<T> {
+        private val cache = hashMapOf<T, T>()
 
-    fun part1(input: List<String>): Int {
-        val valves = readValves(input)
-        val usefulValves = valves.values.filter { it.flowRate > 0 }.toSet()
-        var states = listOf(State(valves.getValue("AA"), 30, 0, usefulValves))
+        operator fun get(value: T) = cache.computeIfAbsent(value) { value }
+    }
+
+    fun calculateMaxReleasedPressure(valves: Map<String, Valve>, remainingMinutes: Int, numberOfTraversers: Int): Int {
+        val pathFinder = PathFinder()
+        val setCache = ObjectPool<Set<Valve>>()
+
+        data class TraverserState(val currentValve: Valve, val remainingMinutes: Int)
+        data class State(
+            val traversers: List<TraverserState>,
+            val releasedPressure: Int,
+            val remainingValves: Set<Valve>
+        )
+
+        val start = valves.getValue("AA")
+        val usefulValves = valves.values.asSequence().filter { it.flowRate > 0 }.toSet()
+        var states = TraverserState(start, remainingMinutes).let {
+            listOf(State(List(numberOfTraversers) { _ -> it }, 0, usefulValves))
+        }
         var maxReleasedPressure = 0
+
         while (states.isNotEmpty()) {
-            states = states.flatMap { state ->
-                val (current, remainingMinutes, releasedPressure, remainingValves) = state
+            states = states.asSequence().filter {
+                it.remainingValves.isNotEmpty() && it.traversers.any { t -> t.remainingMinutes > 0 }
+            }.flatMap { state ->
+                val (traversers, releasedPressure, remainingValves) = state
+                val orderedTraversers = traversers.sortedByDescending { it.remainingMinutes }
+                val next = orderedTraversers.first()
+                val rest = orderedTraversers.drop(1)
                 remainingValves.map {
-                    val path = findPath(current, it)
-                    if (path.size > remainingMinutes) {
-                        state.copy(remainingMinutes = 0)
+                    val path = pathFinder.findPath(next.currentValve, it)
+                    if (path.size > next.remainingMinutes) {
+                        state.copy(traversers = emptyList())
                     } else {
                         val valve = path.last()
-                        val minutes = remainingMinutes - path.size
+                        val minutes = next.remainingMinutes - path.size
                         val pressure = releasedPressure + valve.flowRate * minutes
-                        State(valve, minutes, pressure, remainingValves - valve)
+                        State(
+                            listOf(TraverserState(valve, minutes)) + rest,
+                            pressure,
+                            setCache[remainingValves - valve]
+                        )
                     }
                 }
-            }
-            maxReleasedPressure = max(maxReleasedPressure, states.maxOf { it.pressure })
-            states = states.filter { it.remainingMinutes > 0 && it.remainingValves.isNotEmpty() }
+            }.filter {
+                it.releasedPressure + (it.traversers.maxOfOrNull { t -> t.remainingMinutes - 1 }
+                    ?: 0) * it.remainingValves.sumOf { v -> v.flowRate } > maxReleasedPressure
+            }.toList()
+            maxReleasedPressure = max(maxReleasedPressure, states.maxOfOrNull { it.releasedPressure } ?: 0)
         }
         return maxReleasedPressure
     }
 
-//    fun part2(input: List<String>): Int
+    fun part1(input: List<String>): Int = calculateMaxReleasedPressure(readValves(input), 30, 1)
+
+    fun part2(input: List<String>): Int = calculateMaxReleasedPressure(readValves(input), 26, 2)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day16_test")
     check(part1(testInput) == 1651)
-//    check(part2(testInput) == 1707)
+    check(part2(testInput) == 1707)
 
     val input = readInput("Day16")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
