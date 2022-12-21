d70d884c7adafad8e24ec4eb01b7fb444e03f102
Marcel Joss <marceljoss@hotmail.ch>
2022-12-21 08:52:17 +0100

    Day 20 part 2

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,65 +1,42 @@
-import kotlin.math.abs
-import kotlin.math.sign
-
 fun main() {
     // used to allow finding the right instance of a number with instanceOf
-    class IdentityInt(val value: Int) {
+    class IdentityLong(val value: Long) {
         override fun toString(): String = value.toString()
     }
 
-    /*
-    1, 2, 4
-
-    2, 1, 4
-
-    1, 2, 4
-    1, 4, 2
-
-    1, 2, 4
-    4, 2, 1
-    2, 4, 1
-    2, 1, 4
-
-     */
-
-    fun part1(input: List<String>): Int {
-        val numbers = input.map { IdentityInt(it.toInt()) }
+    fun decrypt(numbers: List<IdentityLong>, times: Int): Long {
         val workingNumbers = numbers.toMutableList()
-        for (n in numbers) {
-            val step = n.value.sign
-            var index = workingNumbers.indexOf(n)
 
-            repeat(abs(n.value % numbers.lastIndex)) {
-                var swapIndex = index + step
-                if (swapIndex == -1) {
-                    swapIndex = workingNumbers.lastIndex
+        repeat(times) {
+            for (n in numbers) {
+                val index = workingNumbers.indexOf(n)
+                var newIndex = ((index + n.value) % workingNumbers.lastIndex).toInt()
+                if (newIndex <= 0) {
+                    newIndex += workingNumbers.lastIndex
                 }
-                if (swapIndex == workingNumbers.size) {
-                    swapIndex = 0
-                }
-                workingNumbers[index] = workingNumbers[swapIndex]
-                workingNumbers[swapIndex] = n
-                index = swapIndex
+
+                if (index == newIndex) continue
+
+                workingNumbers.removeAt(index)
+                workingNumbers.add(newIndex, n)
             }
         }
-        println(workingNumbers)
-        val indexOf0 = workingNumbers.indexOfFirst { it.value == 0 }
-        // -132
-        // -780
-        // 7552
+        val indexOf0 = workingNumbers.indexOfFirst { it.value == 0L}
         return listOf(1000, 2000, 3000).sumOf {
-            println(workingNumbers[(indexOf0 + it) % numbers.size])
             workingNumbers[(indexOf0 + it) % numbers.size].value
         }
     }
 
+    fun part2(input: List<String>): Long = decrypt(input.map { IdentityLong(it.toLong() * 811589153) }, 10)
+
+    fun part1(input: List<String>): Long = decrypt(input.map { IdentityLong(it.toLong()) }, 1)
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day20_test")
-    check(part1(testInput) == 3)
-    part1(listOf("1", "2", "3"))
-//    check(part2(testInput) == 58)
+    check(part1(testInput) == 3L)
+    check(part2(testInput) == 1623178306L)
 
     val input = readInput("Day20")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
